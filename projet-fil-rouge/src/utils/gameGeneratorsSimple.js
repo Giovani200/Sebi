ccbdc074169a7aab5d2d12e9907db47f7a68512c
Giovani200 <giovanikloussey@gmail.com>
2025-06-10 02:29:29 +0200

    Fusion résolue entre masti

--- projet-fil-rouge/src/utils/gameGeneratorsSimple.js
+++ projet-fil-rouge/src/utils/gameGeneratorsSimple.js
@@ -1,5 +1,5 @@
 // Fonction pour générer un nouvel obstacle (uniquement des buissons)
-function generateObstacle(canvas, currentDistance) {
+function generateObstacle(canvas,) {
   const FLOOR_Y = canvas.height - canvas.height * 0.12;
   const OBSTACLE_HEIGHT = canvas.height * 0.14;
   const OBSTACLE_WIDTH = OBSTACLE_HEIGHT * 0.9; // Légèrement plus large pour une hitbox cohérente
